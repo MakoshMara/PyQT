59404fc8ff2d3b8b2973b411ef921e470228b3d4
PanovaM <ri5@mail.ru>
2022-01-17 19:04:00 +0700

    ДЗ№6

---
+++ Lesson5/common/utils.py
@@ -0,0 +1,36 @@
+import sys
+import json
+import traceback
+
+from decos import log
+from errors import IncorrectDataRecivedError
+
+
+
+def get_message(client):
+
+    from common.variables import MAX_PACAGE_LEGTH
+    from common.variables import ENCODING
+
+
+    encoded_response = client.recv(MAX_PACAGE_LEGTH)
+    if isinstance(encoded_response, bytes):
+        print(encoded_response)
+        json_response = encoded_response.decode(ENCODING)
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            print(response)
+            return response
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
+
+
+def send_meccage(sock, message):
+    print(f'{message}')
+    if not isinstance(message,dict):
+        raise TypeError
+    from common.variables import ENCODING
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
+
